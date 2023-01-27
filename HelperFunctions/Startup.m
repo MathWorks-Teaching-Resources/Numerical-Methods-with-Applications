f0709876c842d26c21446b3002197ef0ee37b0a0
Emma Smith Zbarsky <esmithzb@mathworks.com>
2023-01-27 14:46:26 -0500

    Fix relative links in approximatingDerivatives and Onramp links

--- HelperFunctions/Startup.m
+++ HelperFunctions/Startup.m
@@ -2,7 +2,7 @@ function Startup
 % Open the overview file
 locDir = pwd;
 if contains(locDir,filesep+"MATLAB Drive")
-    open("Navigation.mlx")
+    open("NavigationOverview.mlx")
 else
     open("Overview.html")
 end
